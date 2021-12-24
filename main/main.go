36441cadfa8febb0d8241e7ec14632fc7f95c52e
KallyDev <kallydev@gmail.com>
2021-12-24 10:51:21 +0800

    docs: add command description

--- main/main.go
+++ main/main.go
@@ -8,11 +8,13 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use: "tier2pool",
+	Use:  "tier2pool",
+	Long: "A mining pool proxy tool, support BTC, ETH, ETC, XMR mining pool, etc. https://github.com/tier2pool/tier2pool",
 }
 
 var serverCmd = &cobra.Command{
-	Use: "server",
+	Use:   "server",
+	Short: "tier2pool server",
 	Run: func(cmd *cobra.Command, args []string) {
 		srv := server.New(cmd)
 		if err := srv.Run(); err != nil {
@@ -22,7 +24,8 @@ var serverCmd = &cobra.Command{
 }
 
 var clientCmd = &cobra.Command{
-	Use: "client",
+	Use:   "client",
+	Short: "tier2pool client",
 	Run: func(cmd *cobra.Command, args []string) {
 		cli := client.New(cmd)
 		if err := cli.Run(); err != nil {
@@ -37,11 +40,11 @@ func init() {
 		FullTimestamp: true,
 	})
 	serverCmd.Flags().String("token", "", "server access token")
-	serverCmd.Flags().Bool("debug", false, "")
-	serverCmd.Flags().String("listen", "0.0.0.0:443", "")
-	serverCmd.Flags().String("redirect", "", "")
-	serverCmd.Flags().String("ssl-certificate", "", "")
-	serverCmd.Flags().String("ssl-certificate-key", "", "")
+	serverCmd.Flags().Bool("debug", false, "enable debug mode")
+	serverCmd.Flags().String("listen", "0.0.0.0:443", "server listener address")
+	serverCmd.Flags().String("redirect", "", "redirect url for invalid requests")
+	serverCmd.Flags().String("ssl-certificate", "", "ssl certificate")
+	serverCmd.Flags().String("ssl-certificate-key", "", "ssl certificate private key")
 	if err := serverCmd.MarkFlagRequired("ssl-certificate"); err != nil {
 		logrus.Fatal(err)
 	}
@@ -51,11 +54,11 @@ func init() {
 	if err := serverCmd.MarkFlagRequired("token"); err != nil {
 		logrus.Fatal(err)
 	}
-	clientCmd.Flags().Bool("debug", false, "")
-	clientCmd.Flags().String("server", "", "")
-	clientCmd.Flags().String("pool", "", "")
-	clientCmd.Flags().String("token", "", "")
-	clientCmd.Flags().String("listen", "127.0.0.1:1234", "")
+	clientCmd.Flags().Bool("debug", false, "enable debug mode")
+	clientCmd.Flags().String("server", "", "tier2pool server address")
+	clientCmd.Flags().String("pool", "", "mining pool address")
+	clientCmd.Flags().String("token", "", "server access token")
+	clientCmd.Flags().String("listen", "127.0.0.1:1234", "client listener address")
 	if err := clientCmd.MarkFlagRequired("pool"); err != nil {
 		logrus.Fatal(err)
 	}
